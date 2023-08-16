ca34e1d0d52d04a53683a7c16605f44ff68146d2
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-08-15 21:38:41 -0300

    adcionado endpoint de login

--- converter/urls.py
+++ converter/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # add Login to path
+    path("login/", include("apps.auth_credentials.urls")),
 ]
