05be4f0d1cb75bbc19a3d9445622e20f29192bbc
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-08-15 21:38:27 -0300

    iniciado endpoint para login

--- apps/auth_credentials/api/viewsets.py
+++ apps/auth_credentials/api/viewsets.py
@@ -1,19 +1,42 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from apps.auth_credentials.models import User
 
-class AuthViewSet(ModelViewSet):
+from .serializers import AuthSerializer
+
+
+class LoginViewSet(ModelViewSet):
     # add auth
-    authentication_classes = (TokenAuthentication,)
+    # authentication_classes = (TokenAuthentication,)
 
     # add permission
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
+
+    # query
+    queryset = User.objects.all()
+
+    # define serializer
+    serializer_class = AuthSerializer
+
+    # pagination
+    pagination_class = LimitOffsetPagination
 
     def create(self, request, *args, **kwargs):
         # get user and password sent
-        user = request.data["user"]
+        username = request.data["user"]
         password = request.data["password"]
+
+        # get user
+        user = User.objects.get(username=username)
+
+        # check user
+        if not User.objects.get(username=username):
+            return Response("invalid user")
+        elif user.password != password:
+            return Response("Invalid password")
 
         return super().create(request, *args, **kwargs)
