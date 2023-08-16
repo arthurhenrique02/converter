ebd0391e74569da73ed922bb31aaafaa4e662ca4
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-08-16 16:10:09 -0300

    retirado decouple e substituido pela builtin 'os'

--- converter/settings.py
+++ converter/settings.py
@@ -1,6 +1,5 @@
+import os
 from pathlib import Path
-
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,10 +74,10 @@ DATABASES = {
     "auth_db": {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'authdb',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("SQL_HOST"),
+        'PORT': os.environ.get("SQL_PORT"),
     },
 }
 
