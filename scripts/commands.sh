773f71c4980b4fc8586b2c3e395ee1884b4f1373
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-08-21 22:41:18 -0300

    retirado linha de comentario que estava gerando erro

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,19 +1,21 @@
-# try to run as /bin/sh on linux
 #!/bin/sh
 
 # ends shell terminal if an error occurs
 set -e
 
-
-# wait until db startup
-while ! nc -z $SQL_HOST $SQL_PORT; do
-  echo "🟡 Waiting for MYSQL Database Startup ($SQL_HOST $SQL_PORT) ..."
-  sleep 2
-done
-
 echo "✅ MYSQL Database Started Successfully ($SQL_HOST:$SQL_PORT)"
 
-python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
+# Executa as migrações do Django
+echo "🟢 Running Django Migrations..."
+
+# make authdb migrations
+/venv/bin/python manage.py makemigrations --noinput
+/venv/bin/python manage.py migrate --database=auth_db --noinput
+
+# Coleta os arquivos estáticos do Django
+echo "🟢 Collecting Django Static Files..."
+/venv/bin/python manage.py collectstatic --noinput
+
+# Inicia o servidor Django
+echo "🟢 Starting Django Server..."
+/venv/bin/python manage.py runserver 0.0.0.0:8000
