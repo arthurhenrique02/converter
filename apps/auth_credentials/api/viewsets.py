70e26c0ca9bea3e136a47a9fd3ed7dac54fd4bbd
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-09-09 03:15:55 -0300

    criado POST methods para log out e registro

--- apps/auth_credentials/api/viewsets.py
+++ apps/auth_credentials/api/viewsets.py
@@ -1,46 +1,96 @@
-from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import CreateAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework.viewsets import ViewSet
 
 from .serializers import AuthSerializer
 
 
-class LoginViewSet(generics.CreateAPIView):
-    # add auth
-    # authentication_classes = (TokenAuthentication,)
-
-    # add permission
-    # permission_classes = (IsAuthenticated,)
-
+class LoginViewSet(CreateAPIView, ViewSet):
     # query
     queryset = User.objects.all()
 
     # define serializer
     serializer_class = AuthSerializer
 
-    # pagination
-    pagination_class = LimitOffsetPagination
+    # pagination_class = LimitOffsetPagination
+    # overrid POST method to get login credentials
 
     def create(self, request, *args, **kwargs):
         # get user and password sent
-        username = request.data["user"]
-        password = request.data["password"]
+        try:
+            username = request.data["username"]
+            password = request.data["password"]
+        except:
+            return Response(
+                {"message": "Missing credentials (username or password)"}
+            )
 
         # authenticate user
         user = authenticate(username=username, password=password)
 
         # check if user`s invalid
         if not user:
-            print("INVALID")
-            raise ValidationError("Invalid USERNAME or PASSWORD")
-    
-        # by default, just login
+            return Response({"message": "Invalid USERNAME or PASSWORD"}, 404)
 
-        return Response("logged")
-        # return super().create(request, *args, **kwargs)
+        # by default, just login
+        return Response({"message": "Logged in"}, 200)
+
+
+class LogoutViewSet(CreateAPIView, ViewSet):
+    # query
+    queryset = User.objects.all()
+
+    # define serializer
+    serializer_class = AuthSerializer
+
+    pagination_class = LimitOffsetPagination
+
+    # override POST method to just logout
+    def create(self, request, *args, **kwargs):
+        logout(request=request)
+
+        # by default, just login
+        return Response({"message": "Logged out"}, 200)
+
+
+class CreateUserViewSet(CreateAPIView, ViewSet):
+    # query
+    queryset = User.objects.all()
+
+    # define serializer
+    serializer_class = AuthSerializer
+
+    pagination_class = LimitOffsetPagination
+
+    # override POST
+    def create(self, request, *args, **kwargs):
+        # get user and password sent
+        username = request.data["username"]
+        email = request.data["email"]
+        password = request.data["password"]
+
+        # check sent credentials
+        if not username:
+            return Response({"message": "Missing user"}, 404)
+
+        if not email:
+            return Response({"message": "Missing email"}, 404)
+
+        if not password:
+            return Response({"message": "Missing password"}, 404)
+
+        # create and save new user instance
+        new_user = User.objects.create(
+            username=username
+        )
+        new_user.set_password(password)
+        new_user.email = email
+        new_user.save()
+
+        # create_auth_token.send(new_user, created=True)
+        return Response({"message": "User created"}, 200)
