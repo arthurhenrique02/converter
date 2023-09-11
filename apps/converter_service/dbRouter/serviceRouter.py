29d78d335950a0f47d4c285e002471ec5c763bd9
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-09-11 15:47:18 -0300

    criado router para a app converter_service

---
+++ apps/converter_service/dbRouter/serviceRouter.py
@@ -0,0 +1,50 @@
+class ServiceRouter:
+    """
+    A router to control all database operations on models in the
+    converter service application.
+    """
+
+    # to check app`s name labels
+    route_app_labels = {"converter_service"}
+
+    def db_for_read(self, model, **hints):
+        """
+        Attempts to read converter_service model go to files_db.
+        """
+        # check`s if app name == auth
+        if model._meta.app_label in self.route_app_labels:
+            # returns access to auth db
+            return "files_db"
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write converter_service model go to files_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            # returns access to auth db
+            return "files_db"
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the converter_service app is
+        involved.
+        """
+        if (
+            obj1._meta.app_label in self.route_app_labels
+            or obj2._meta.app_label in self.route_app_labels
+        ):
+            # gives access to auth db
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the converter_service app only appear in the
+        'files_db' database.
+        """
+        if app_label in self.route_app_labels:
+            # returns access to auth db
+            return db == "files_db"
+        return None
